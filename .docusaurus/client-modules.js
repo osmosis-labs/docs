94043e114b391374bcbaaa183feee2951ba8b2d8
Daniel Farina <contact@nevulas.com>
2022-09-08 10:57:11 -0400

    Fixes

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/web/code/docs-beta/docs/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/web/code/docs-beta/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/web/code/docs-beta/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/web/code/docs-beta/docs/src/css/custom.css'),
-];
