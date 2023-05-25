4281105073761ba8f7e4fd122169b15548ea5ec2
Daniel Farina <contact@nevulas.com>
2023-05-24 19:21:25 -0700

    Update index.jsx

--- src/theme/Footer/Layout/index.jsx
+++ src/theme/Footer/Layout/index.jsx
@@ -20,6 +20,25 @@ export default function FooterLayout({ style, links, logo, copyright }) {
         </div>
         {copyright && <div className="footer__bottom text--center">{copyright}</div>}
       </div>
+         
+<script async src="https://tally.so/widgets/embed.js"></script>
+<script>
+  window.TallyConfig = {
+    "formId": "w8zL9k",
+    "popup": {
+      "width": 900,
+      "emoji": {
+        "text": ":wave:",
+        "animation": "wave"
+      },
+      "layout": "modal",
+      "showOnce": true,
+      "autoClose": 0
+    }
+  };
+</script>
+<script src="https://widget.kapa.ai/kapa-widget.bundle.js" data-website-id="c5b5e9fc-d025-4c12-b08e-9784d0e2161f" data-project-name="Osmosis" data-project-color="#7900B4" data-project-logo="https://app.osmosis.zone/_next/image?url=%2Ftokens%2Fosmo.svg&w=64&q=75" async></script>
+
     </footer>
   );
 }
